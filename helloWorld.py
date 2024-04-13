827487c5cfc4fa9d8bb4481d972d20957fd581e2
starDust <GanYu_996@outlook.com>
2024-04-13 14:21:10 +0800

    有错误的版本，找不到bug

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,15 @@
-print("Hello world!")
-print("\n")
-#what is git
+a=[i for i in range(1,11)]
 
-for i in range(10):
-    print("hello world")
+b=[0 for _ in range(6)]
+
+for i in range(len(a)-1):
+    for j in range(6):
+        if (a[i]>>j) & 1:
+            b[j]+=1
+
+ans=0
+
+for i in range(6):
+    ans+=(len(a)-b[i])*b[i]*(1<<i)
+
+print(ans)
