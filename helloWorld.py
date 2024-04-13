3614ecac0116aa07b6d6fbfdd78fe24db7cb4289
starDust <GanYu_996@outlook.com>
2024-04-13 14:27:47 +0800

    少循环一个数，对了就见鬼了

--- helloWorld.py
+++ helloWorld.py
@@ -1,9 +1,10 @@
 a=[i for i in range(1,11)]
 
-b=[0 for _ in range(6)]
+b=[0 for _ in range(7)]
 
-for i in range(len(a)-1):
-    for j in range(6):
+for i in range(len(a)):
+    
+    for j in range(7):
         if (a[i]>>j) & 1:
             b[j]+=1
 
@@ -12,4 +13,6 @@ ans=0
 for i in range(6):
     ans+=(len(a)-b[i])*b[i]*(1<<i)
 
+print(a)
+print(b)
 print(ans)
